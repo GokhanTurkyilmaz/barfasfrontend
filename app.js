35a994986437baeee74efc0b5199e64aebe76348
Mohammadali  Ghaliany <moalighaliany@gmail.com>
2021-11-03 14:41:38 +0300

    pppp

--- app.js
+++ app.js
@@ -106,6 +106,10 @@ axios(config)
 
 
   })
- app.listen(port, () => {
-   console.log(`Example app listening at http://localhost:${port}`)
+ ///app.listen(port, () => {
+  /// console.log(`Example app listening at http://localhost:${port}`)
+  const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+
  })
